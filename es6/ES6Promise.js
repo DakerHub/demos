4ea83892c9a121d4f234cbfd2f99b6c64cb0f31b
Daker <523246656@qq.com>
2021-04-30 19:23:00 +0800

    test promise

--- es6/ES6Promise.js
+++ es6/ES6Promise.js
@@ -1,38 +1,41 @@
-var p = Promise.resolve(1)
-
-p.then(
-  (val) => {
-    console.log(val)
-  },
-  (val) => {
-    console.log(val)
-  }
-).then((val) => {
-  console.log(val)
+var p = new Promise((resolve, reject) => {
+  resolve({
+    then: function (resolve, reject) {
+      resolve(1)
+    },
+  })
 })
 
-var then = p.then
-console.log(
-  then((val) => {
-    console.log('Resolve: ', val)
-  })
-)
-
-setTimeout(() => {
-  p.then((val) => {
-    console.log('Resolve2: ', val)
-  })
-}, 0)
-
-// Case2 resolve 一个 padding 的 promise
-var p2 = new Promise((resolve, reject) => {
-  resolve(1)
+p.then(function onFulfilled(value) {
+  console.log(value)
 })
 
-p2.then((val) => {
-  return new Promise((resolve) => {
-    resolve(val + 1)
-  })
-}).then((val) => {
-  console.log('Case2: ', val)
-})
+// p.then(
+//   (val) => {
+//     console.log(val)
+//   },
+//   (val) => {
+//     console.log(val)
+//   }
+// ).then((val) => {
+//   console.log(val)
+// })
+
+// setTimeout(() => {
+//   p.then((val) => {
+//     console.log('Resolve2: ', val)
+//   })
+// }, 0)
+
+// // Case2 resolve 一个 padding 的 promise
+// var p2 = new Promise((resolve, reject) => {
+//   resolve(1)
+// })
+
+// p2.then((val) => {
+//   return new Promise((resolve) => {
+//     resolve(val + 1)
+//   })
+// }).then((val) => {
+//   console.log('Case2: ', val)
+// })
